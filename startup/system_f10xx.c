1449a04ba5f06eed1da8b2c8b0bb9a799dc1e750
Alexander Kuzkin <xbevice@gmail.com>
2018-08-26 14:47:32 +0300

    startup f407

--- startup/system_f10xx.c
+++
@@ -1,5 +0,0 @@
-#include <stdint.h>
-
-const uint8_t AHBPrescTable[16U] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
-const uint8_t APBPrescTable[8U] =  {0, 0, 0, 0, 1, 2, 3, 4};
-uint32_t SystemCoreClock;
